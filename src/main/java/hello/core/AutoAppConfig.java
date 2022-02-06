8a7780e564bf7eb8bc7115ab0664a315c83fea44
HunkiKim <gnsrl76@naver.com>
2022-02-07 02:04:14 +0900

    빈 생명주기 콜백 시작

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -8,10 +8,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
 
 @Configuration
-@ComponentScan(
-        basePackages = "hello.core.member",
-        excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
-)
+@ComponentScan
 // AppConfig를 뺄 것이란느 의미
 public class AutoAppConfig
 {
